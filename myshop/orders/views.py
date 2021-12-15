971a3be2b889ece26f4b1fb51f9569d5bce917d9
pravintp <pravin@testpress.in>
2021-12-15 19:17:40 +0530

    Add support to send asynchronous order invoice mail

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
+from .tasks import order_created
 
 # Create your views here.
 
@@ -13,6 +14,7 @@ def order_create(request):
         if form.is_valid():
             order = form.save()
             cart.add_all_items_in_the_cart_to_order_and_clear_the_cart(order)
+            order_created.delay(order.id)
             return render(request, "orders/order/created.html", {"order": order})
     return render(
         request, "orders/order/create.html", {"cart": cart, "form": OrderCreateForm()}
