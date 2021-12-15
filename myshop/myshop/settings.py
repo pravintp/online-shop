72c6ae8d640abea3f9e5513c12116d511fa9bfe4
pravintp <pravin@testpress.in>
2021-12-15 16:59:18 +0530

    Add cart functionality to the shop

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "shop.apps.ShopConfig",
+    "cart.apps.CartConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+
+CART_SESSION_ID = "cart"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
