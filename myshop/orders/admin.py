7f6538b2def23bdf2e5e9475b1dd3f11154fb22c
pravintp <pravin@testpress.in>
2021-12-16 14:24:53 +0530

    Extend admin order detail view to display ordered item details

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -3,6 +3,8 @@ import csv
 import datetime
 from django.http import HttpResponse
 from .models import Order, OrderItem
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 
@@ -38,6 +40,11 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ["product"]
 
 
+def order_detail(obj):
+    url = reverse("orders:admin_order_detail", args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = [
@@ -51,6 +58,7 @@ class OrderAdmin(admin.ModelAdmin):
         "paid",
         "created",
         "updated",
+        order_detail,
     ]
     list_filter = ["paid", "created", "updated"]
     inlines = [OrderItemInline]
