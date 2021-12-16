637946849e8bf78204dc028597f512d62466cb9e
pravintp <pravin@testpress.in>
2021-12-16 12:29:15 +0530

    Add braintree payment gateway

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
@@ -22,7 +23,8 @@ def order_create(request):
                 )
             cart.clear()
             order_created.delay(order.id)
-            return render(request, "orders/order/created.html", {"order": order})
+            request.session["order_id"] = order.id
+            return redirect(reverse("payment:process"))
     return render(
         request, "orders/order/create.html", {"cart": cart, "form": OrderCreateForm()}
     )
