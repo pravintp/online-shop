806a96370d6b5528c2fa1721c0112dd2350dc5ef
pravintp <pravin@testpress.in>
2021-12-16 15:59:51 +0530

    Add support to send pdf order invoce by email

--- myshop/payment/views.py
+++ myshop/payment/views.py
@@ -1,6 +1,8 @@
 import braintree
 from django.shortcuts import render, redirect, get_object_or_404
 from django.conf import settings
+from .tasks import payment_completed
+
 from orders.models import Order
 
 
@@ -26,6 +28,7 @@ def payment_process(request):
             order.paid = True
             order.braintree_id = result.transaction.id
             order.save()
+            payment_completed.delay(order.id)
             return redirect("payment:done")
         else:
             return redirect("payment:canceled")
